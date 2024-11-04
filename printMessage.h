71de3c710ebbc0c6e522cd73f472eaa40305f07f
Shwetha1235 <shwethaam333@gmail.com>
2024-11-04 09:56:18 +0530

    Update printMessage.h

--- printMessage.h
+++ printMessage.h
@@ -1,18 +1,1 @@
-#include <stdio.h>
-#include <string.h>
 
-void printMsg(const char *message) {
-    printf("%s\n", message);
-}
-void printtoMsg(const char *message) {
-    printf("To: %s\n", message);
-}
-
-void printcontroller(const unsigned short header,BreachType breachtype) {
-    printf("%x : %x\n", header, breachtype);
-}
-
-
-void (*printMessage)(const char *message) = printMsg;
-void (*printToMessage)(const char *message) = printtoMsg;
-void (*printtocontroller)(const unsigned short header,BreachType breachtype) = printcontroller;
